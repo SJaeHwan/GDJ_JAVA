838567354426433b59055a31d8e36693058f9341
SJaeHwan <jhseok28@gamil.com>
2023-12-06 10:44:24 +0900

    add

--- src/com/winter/study1/ArrayStudy2.java
+++ src/com/winter/study1/ArrayStudy2.java
@@ -6,7 +6,11 @@ public class ArrayStudy2 {
 		//배열선언
 		int [] ar = {2,4,5};
 			
-		int [] ar2;
+		int [] ar2 =new int[2];
+		
+		for(int i = 0 ; i <ar2.length; i++) {
+			ar2[i]=ar[i];
+		}
 		
 		
 		for(int i = 0; i <ar.length; i++) {
---
+++ src/com/winter/study1/ArrayStudy3.java
@@ -0,0 +1,17 @@
+package com.winter.study1;
+
+public class ArrayStudy3 {
+
+	public static void main(String[] args) {
+		int [] ar = {2,5,4,1,3};
+		
+		
+		
+		System.out.println();
+		
+		
+		
+		
+	}
+
+}
