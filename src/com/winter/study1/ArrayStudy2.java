d311404db4c217c53064f84ea71732b6a0e4f9f8
SJaeHwan <jhseok28@gamil.com>
2023-12-06 10:19:05 +0900

    add

---
+++ src/com/winter/study1/ArrayStudy2.java
@@ -0,0 +1,17 @@
+package com.winter.study1;
+
+public class ArrayStudy2 {
+
+	public static void main(String[] args) {
+		//배열선언
+		int [] ar = {2,4,5};
+			
+		int [] ar2;
+		
+		
+		for(int i = 0; i <ar.length; i++) {
+			System.out.println(ar[i]);
+		}
+	}
+
+}
